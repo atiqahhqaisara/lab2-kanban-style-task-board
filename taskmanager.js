7abe28e68114ac231aab3c39ca657af58442991a
atiqahhqaisara <atiqahqaisarah@gmail.com>
2026-04-13 09:37:43 +0800

    task card + add task on .js, update style.css

--- taskmanager.js
+++ taskmanager.js
@@ -3,3 +3,76 @@ let currentId = 0;
 let editingId = null;
 
 const taskCount = document.getElementById('task-count');
+
+function createTaskCard(task) {
+  const li = document.createElement('li');
+  li.classList.add('task-card');
+  li.setAttribute('data-id', task.id);
+  li.setAttribute('data-priority', task.priority);
+
+  // TITLE
+  const title = document.createElement('span');
+  title.textContent = task.title;
+  title.classList.add('task-title');
+
+  // DOUBLE CLICK EDIT
+  title.addEventListener('dblclick', function () {
+    const input = document.createElement('input');
+    input.value = title.textContent;
+
+    input.addEventListener('blur', saveEdit);
+    input.addEventListener('keypress', function (e) {
+      if (e.key === 'Enter') saveEdit();
+    });
+
+    function saveEdit() {
+      title.textContent = input.value;
+      li.replaceChild(title, input);
+    }
+
+    li.replaceChild(input, title);
+  });
+
+  // DESCRIPTION
+  const desc = document.createElement('p');
+  desc.textContent = task.description;
+
+  // PRIORITY
+  const priority = document.createElement('span');
+  priority.textContent = task.priority;
+
+  // DATE
+  const date = document.createElement('small');
+  date.textContent = task.dueDate;
+
+  // EDIT BUTTON
+  const editBtn = document.createElement('button');
+  editBtn.textContent = 'Edit';
+  editBtn.setAttribute('data-action', 'edit');
+  editBtn.setAttribute('data-id', task.id);
+
+  // DELETE BUTTON
+  const delBtn = document.createElement('button');
+  delBtn.textContent = 'Delete';
+  delBtn.setAttribute('data-action', 'delete');
+  delBtn.setAttribute('data-id', task.id);
+
+  li.appendChild(title);
+  li.appendChild(desc);
+  li.appendChild(priority);
+  li.appendChild(date);
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
+
+  return li;
+}
+
+function addTask(columnId, task) {
+  const column = document.querySelector(`#${columnId} .task-list`);
+  const card = createTaskCard(task);
+
+  column.appendChild(card);
+  tasks.push(task);
+
+  updateCounter();
+}
